00b8d43c388a094397e46106092d47d83e3214d1
YeongJunKim <dud3722000@naver.com>
2019-05-28 23:39:19 +0900

    line alignment & add topics in class

--- stm32cubeide/stm32f446re_test/Inc/ROS/aucar_msgs/AuCAR.h
+++ stm32cubeide/stm32f446re_test/Inc/ROS/aucar_msgs/AuCAR.h
@@ -21,46 +21,50 @@
 
 namespace aucar_msgs
 {
-	class AuCAR : public ros::Msg
-	{
-	public:
-		typedef std_msgs::Header _header_type;
-		_header_type header;
-		typedef std_msgs::String _string_type;
-		_string_type string;
-		typedef ros::Time _time_ref_type;
-		_time_ref_type time_ref;
-		typedef const char* _source_type;
-		_source_type source;
+  class AuCAR : public ros::Msg
+  {
+  public:
+    typedef std_msgs::Header _header_type;
+    _header_type header;
+    typedef std_msgs::String _string_type;
+    _string_type string;
 
-		AuCAR():
-			header(),
-			string(),
-			time_ref(),
-			source("")
-		{
+    AuCAR () :
+	header (), string ()
+    {
 
-		}
+    }
 
-		virtual int serialize(unsigned char *outbuffer) const
-		{
-			int offset = 0;
-			offset += this->header.serialize(outbuffer + offset);
-			offset += this->string.serialize(outbuffer + offset);
-			return offset;
-		}
-		virtual int deserialize(unsigned char *inbuffer)
-		{
-			int offset = 0;
-			offset += this->header.deserialize(inbuffer + offset);
-			offset += this->string.deserialize(inbuffer + offset);
-			return offset;
-		}
+    virtual int
+    serialize (unsigned char *outbuffer) const
+    {
+      int offset = 0;
+      offset += this->header.serialize (outbuffer + offset);
+      offset += this->string.serialize (outbuffer + offset);
+      return offset;
+    }
+    virtual int
+    deserialize (unsigned char *inbuffer)
+    {
+      int offset = 0;
+      offset += this->header.deserialize (inbuffer + offset);
+      offset += this->string.deserialize (inbuffer + offset);
+      return offset;
+    }
 
-		const char * getType(){ return "aucar_msgs/AuCAR"; };
-		const char * getMD5(){ return "11111111110000000000111111111122"; };
-	};
+    const char *
+    getType ()
+    {
+      return "aucar_msgs/AuCAR";
+    }
+    ;
+    const char *
+    getMD5 ()
+    {
+      return "11111111110000000000111111111122";
+    }
+    ;
+  };
 }
-
 
 #endif /* AUCAR_H_ */
